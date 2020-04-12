58ef5a72d4036293745964695d5459ae6b76014c
shreyas chincholkar <shreyaschincholkar@gmail.com>
2020-04-12 19:59:43 +0530

    searched Results functionality

--- Main Website/courses.php
+++ Main Website/courses.php
@@ -77,29 +77,7 @@
 	
 	<!-- search section end -->
 
-
-	<!-- course section -->
-	<section class="course-section spad pb-0">
-		<!-- <div class="course-warp">
-			<ul class="course-filter controls">
-				<li class="control active" data-filter="all">All</li>
-				<li class="control" data-filter=".finance">Finance</li>
-				<li class="control" data-filter=".design">Design</li>
-				<li class="control" data-filter=".web">Web Development</li>
-				<li class="control" data-filter=".photo">Photography</li>
-			</ul>                                        -->
-			<div class="row course-items-area">
-				<!-- course -->
-				<div class="mix col-lg-3 col-md-4 col-sm-6 finance">
-					<div class="course-item">
-						<div class="course-thumb set-bg" data-setbg="img/courses/1.jpg">
-							
-						</div>
-						<div class="course-info">
-							<div class="course-text">
-								<h5>
-								<!-- Edited code -->
-								<?php 
+	<?php 
 								//echo $_POST["name"];
 								$servername = "localhost";
 								$username = "root";
@@ -120,159 +98,56 @@
 								
 								$result1  = $conn->query($sql);
 								$result2  = $conn->query($sql2);
-								//$result3  = $conn->query($sql3);
-								 
-								if($row = $result2->fetch_assoc())
-								{
-									echo " ". $row["fact_name"] ." ";
-								}
+	?>
+
+	<!-- Results section -->
+	<section class="course-section spad pb-0">
+			<div class="row course-items-area">
+		                          
+				<?php
+				
+				while($row = $result2->fetch_assoc())
+				{
+				?>
+				<!-- TO DO -->
+					<div class="mix col-lg-3 col-md-4 col-sm-6 photo">
+						<div class="course-item">
+							<div class="course-thumb set-bg" data-setbg="img/courses/8.jpg">
 							
-								?>
-								 <!--End  -->
-								</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur</p>
-								<div class="students">120 Students</div>
 							</div>
-							<div class="course-author">
-								<div class="ca-pic set-bg" data-setbg="img/authors/1.jpg"></div>
-								<p>William Parker, <span>Developer</span></p>
+							<div class="course-info">
+								<div class="course-text">
+									<h5>
+									<?php 
+									echo "".$row["fact_name"]." ";
+									
+									?>
+									</h5>
+									<p>Lorem ipsum dolor sit amet, consectetur</p>
+									<div class="students">120 Students</div>
+								</div>
+								<div class="course-author">
+									<div class="ca-pic set-bg" data-setbg="img/authors/8.jpg"></div>
+									<p>William Parker, <span>Developer</span></p>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
+				<?php
+				}	
+				?>
 				<!-- course -->
-				<div class="mix col-lg-3 col-md-4 col-sm-6 design">
-					<div class="course-item">
-						<div class="course-thumb set-bg" data-setbg="img/courses/2.jpg">
-							
-						</div>
-						<div class="course-info">
-							<div class="course-text">
-								<h5>IT Development</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur</p>
-								<div class="students">120 Students</div>
-							</div>
-							<div class="course-author">
-								<div class="ca-pic set-bg" data-setbg="img/authors/2.jpg"></div>
-								<p>William Parker, <span>Developer</span></p>
-							</div>
-						</div>
-					</div>
-				</div>
+				
 				<!-- course -->
-				<div class="mix col-lg-3 col-md-4 col-sm-6 web">
-					<div class="course-item">
-						<div class="course-thumb set-bg" data-setbg="img/courses/3.jpg">
-							
-						</div>
-						<div class="course-info">
-							<div class="course-text">
-								<h5>Graphic Design</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur</p>
-								<div class="students">120 Students</div>
-							</div>
-							<div class="course-author">
-								<div class="ca-pic set-bg" data-setbg="img/authors/3.jpg"></div>
-								<p>William Parker, <span>Developer</span></p>
-							</div>
-						</div>
-					</div>
-				</div>
+				
 				<!-- course -->
-				<div class="mix col-lg-3 col-md-4 col-sm-6 photo">
-					<div class="course-item">
-						<div class="course-thumb set-bg" data-setbg="img/courses/4.jpg">
-							
-						</div>
-						<div class="course-info">
-							<div class="course-text">
-								<h5>IT Development</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur</p>
-								<div class="students">120 Students</div>
-							</div>
-							<div class="course-author">
-								<div class="ca-pic set-bg" data-setbg="img/authors/4.jpg"></div>
-								<p>William Parker, <span>Developer</span></p>
-							</div>
-						</div>
-					</div>
-				</div>
+			
 				<!-- course -->
-				<div class="mix col-lg-3 col-md-4 col-sm-6 finance">
-					<div class="course-item">
-						<div class="course-thumb set-bg" data-setbg="img/courses/5.jpg">
-							
-						</div>
-						<div class="course-info">
-							<div class="course-text">
-								<h5>IT Development</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur</p>
-								<div class="students">120 Students</div>
-							</div>
-							<div class="course-author">
-								<div class="ca-pic set-bg" data-setbg="img/authors/5.jpg"></div>
-								<p>William Parker, <span>Developer</span></p>
-							</div>
-						</div>
-					</div>
-				</div>
+				
 				<!-- course -->
-				<div class="mix col-lg-3 col-md-4 col-sm-6 design">
-					<div class="course-item">
-						<div class="course-thumb set-bg" data-setbg="img/courses/6.jpg">
-						
-						</div>
-						<div class="course-info">
-							<div class="course-text">
-								<h5>Socia Media</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur</p>
-								<div class="students">120 Students</div>
-							</div>
-							<div class="course-author">
-								<div class="ca-pic set-bg" data-setbg="img/authors/6.jpg"></div>
-								<p>William Parker, <span>Developer</span></p>
-							</div>
-						</div>
-					</div>
-				</div>
+				
 				<!-- course -->
-				<div class="mix col-lg-3 col-md-4 col-sm-6 web">
-					<div class="course-item">
-						<div class="course-thumb set-bg" data-setbg="img/courses/7.jpg">
-							
-						</div>
-						<div class="course-info">
-							<div class="course-text">
-								<h5>IT Development</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur</p>
-								<div class="students">120 Students</div>
-							</div>
-							<div class="course-author">
-								<div class="ca-pic set-bg" data-setbg="img/authors/7.jpg"></div>
-								<p>William Parker, <span>Developer</span></p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- course -->
-				<div class="mix col-lg-3 col-md-4 col-sm-6 photo">
-					<div class="course-item">
-						<div class="course-thumb set-bg" data-setbg="img/courses/8.jpg">
-							
-						</div>
-						<div class="course-info">
-							<div class="course-text">
-								<h5>HTML 5</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur</p>
-								<div class="students">120 Students</div>
-							</div>
-							<div class="course-author">
-								<div class="ca-pic set-bg" data-setbg="img/authors/8.jpg"></div>
-								<p>William Parker, <span>Developer</span></p>
-							</div>
-						</div>
-					</div>
-				</div>
+			
 			</div>
 			
 		</div>
